cca892b2824920c357e266af45ae9cb3661041ed
Julio Toboso <JulioToboso@gmail.com>
2024-09-17 23:20:21 +0200

    Update chat_client.adb

--- client/chat_client.adb
+++ client/chat_client.adb
@@ -1,22 +1,17 @@
---gnatmake -I/usr/local/ll/lib chat_client.adb
---./chat_client <Machine> <Port> <Client's Nick>
-
-with Ada.Text_IO;
-with Ada.Command_Line;
-with Ada.Strings.Unbounded;
-with Ada.Exceptions;
-
-with Chat_Control;
-with Chat_Messages;
-with Lower_Layer_UDP;
 with Handler_Client;
+with Lower_Layer_UDP;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
+with Ada.Exceptions;
+with Chat_Messages;
+with Ada.Command_Line;
 
-Procedure Chat_Client is
+procedure Chat_Client is
     package LLU renames Lower_Layer_UDP;
     package ASU renames Ada.Strings.Unbounded;
-    package T_IO renames Ada.Text_IO;
+    package ATI renames Ada.Text_IO;
     package CM renames Chat_Messages;
-    package ComL renames Ada.Command_Line;
+    package ACL renames Ada.Command_Line;
 
     Usage_Error: exception;
 
@@ -32,42 +27,44 @@ Procedure Chat_Client is
     Fin_Mensajes: Boolean := False;
     Etiqueta: CM.Message_Type;
 
-    -- Helper function to reset and send buffer
+    -- Helper procedure to reset and send the buffer
     procedure Send_Buffer(Buffer: in out LLU.Buffer_Type; EP: LLU.End_Point_Type) is
     begin
         LLU.Reset(Buffer);
         LLU.Send(EP, Buffer'Access);
     end Send_Buffer;
 
-    -- Modular Read Procedure
+    -- Read procedure for receiving messages from the server
     procedure Read_Procedure is
     begin
         LLU.Receive(Client_EP_Receive, Buffer'Access, 10.0, Expired);
         if Expired then
-            T_IO.Put_Line("Timeout or no message received.");
+            ATI.Put_Line("Timeout: No response from the server.");
         else
             Etiqueta := CM.Message_Type'Input(Buffer'Access);
             Acogido := Boolean'Input(Buffer'Access);
             if Acogido then
-                T_IO.Put_Line("Mini-Chat: Welcome " & ASU.To_String(Nick));
+                ATI.Put_Line("Mini-Chat: Welcome " & ASU.To_String(Nick));
             else
-                T_IO.Put_Line("Nick already used, connection rejected.");
+                ATI.Put_Line("Mini-Chat: Nickname already in use. Connection rejected.");
             end if;
         end if;
     end Read_Procedure;
 
-    -- Modular Write Procedure
+    -- Write procedure for sending messages to the server
     procedure Write_Procedure is
     begin
         while not Fin_Mensajes loop
-            T_IO.Put(">> ");
-            Comentario := ASU.To_Unbounded_String(T_IO.Get_Line);
+            ATI.Put(">> ");
+            Comentario := ASU.To_Unbounded_String(ATI.Get_Line);
             if ASU.To_String(Comentario) = ".quit" then
+                -- Sending a logout message and exit
                 CM.Message_Type'Output(Buffer'Access, CM.Logout);
                 ASU.Unbounded_String'Output(Buffer'Access, Nick);
                 Send_Buffer(Buffer, Server_EP);
                 Fin_Mensajes := True;
             else
+                -- Sending a regular chat message
                 CM.Message_Type'Output(Buffer'Access, CM.Writer);
                 ASU.Unbounded_String'Output(Buffer'Access, Nick);
                 ASU.Unbounded_String'Output(Buffer'Access, Comentario);
@@ -78,26 +75,26 @@ Procedure Chat_Client is
 
 begin
     -- Input validation
-    if ComL.Argument_Count /= 3 or else ComL.Argument(3) = "server" then
+    if ACL.Argument_Count /= 3 or else ACL.Argument(3) = "server" then
         raise Usage_Error;
     else
-        Nick := ASU.To_Unbounded_String(ComL.Argument(3));
-        Server_EP := LLU.Build(LLU.To_IP(ComL.Argument(1)), Integer'Value(ComL.Argument(2)));
+        Nick := ASU.To_Unbounded_String(ACL.Argument(3));
+        Server_EP := LLU.Build(LLU.To_IP(ACL.Argument(1)), Integer'Value(ACL.Argument(2)));
     end if;
 
-    -- Setup the client endpoint with handler and receiving
+    -- Binding the handler and receive endpoints
     LLU.Bind_Any(Client_EP_Handler, Handler_Client.Client_Handler'Access);
     LLU.Bind_Any(Client_EP_Receive);
 
-    -- Initialize and send join message to the server
+    -- Send initialization message to the server
     CM.Message_Type'Output(Buffer'Access, CM.Init);
     ASU.Unbounded_String'Output(Buffer'Access, Nick);
     Send_Buffer(Buffer, Server_EP);
 
-    -- Read and process server response
+    -- Wait for server response
     Read_Procedure;
 
-    -- If accepted, proceed to the chat loop
+    -- If the server accepted, start the chat loop
     if Acogido then
         Write_Procedure;
     end if;
@@ -107,10 +104,11 @@ begin
 
 exception
     when Usage_Error =>
-        T_IO.Put_Line("Usage Error: Provide <Server IP> <Port> <Nick>.");
+        ATI.Put_Line("Usage: " & ACL.Command_Name & " <Server IP> <Port> <Nick> (Nick should not be 'server')");
         LLU.Finalize;
     when others =>
-        T_IO.Put_Line("Unexpected exception: " & Ada.Exceptions.Exception_Name(others) &
-                      " - " & Ada.Exceptions.Exception_Message(others));
+        Ada.Text_IO.Put_Line("Unexpected error: " &
+                             Ada.Exceptions.Exception_Name(others) & " - " &
+                             Ada.Exceptions.Exception_Message(others));
         LLU.Finalize;
 end Chat_Client;
